72da1fc5e6a589de3e6af23a98d79994da815380
dgrechin <dgrechin@gmail.com>
2018-12-20 10:03:23 +0200

    Сделал только 1-6 задание полностью, 7 начал

--- Lesson7/FormTask7.php
+++ Lesson7/FormTask7.php
@@ -1,17 +1,20 @@
 <?php
 
 namespace App;
-
 header('Content-Type: text/plain; charset=utf-8');
 class FormTask7
 {
-public $input;
-public $submit;
-public $password;
-public $textarea;
-public $open;
-public $close;
+    public  $open= '<form method="POST"><br>';
+    public  $input= '<input type="text" placeholder="Ваше имя" name="name"><br>';
+    public $submit;
+    public $password='<input type="password" placeholder="password" name="pass"><br>';
+    public $textarea='<textarea> type="text" placeholder="123" name="textarea"</textarea><br>';
+    public $close = '</form>';
 }
 $form= new FormTask7();
-$form->input= ['type'=>'text', 'value'=>'!!!' , 'class'=>'ggg'];
-echo $form->input[1];
+echo $form->open;
+echo $form->input;
+echo $form->password;
+echo $form->textarea;
+echo $form->close;
+
